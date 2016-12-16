5d6f2b442f8e9ef559390dbc94df098fbc8a6a35
Sofia Celi <sceli@thoughtworks.com>
2016-12-16 16:59:19 -0500

    Y & S: refactors copy method and defines constants

--- decaf.go
+++ decaf.go
@@ -2,13 +2,18 @@ package ed448
 
 const (
 	// D is the non-square element of F_p
-	d = -39081
-
+	d                = -39081
 	montgomeryFactor = "3bd440fae918bc5ull"
+	lbits            = 28
+	lmask            = (limb(1) << lbits) - 1
 )
 
-// XXX: Will bigNumber change with Decaf?
-//type decaf_word_t uint32
+// Copy copies n = y
+func (n *bigNumber) decafCopy() *bigNumber {
+	c := &bigNumber{}
+	copy(c[:], n[:])
+	return c
+}
 
 //type gf struct {
 //	limb [16]decaf_word_t
