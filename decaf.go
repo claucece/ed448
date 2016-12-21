ab2e120177b0568cd15d7813d63d8f96dcffe198
Sofia Celi <sceli@thoughtworks.com>
2016-12-21 12:53:22 -0500

    Y & S: refactors methods

--- decaf.go
+++ decaf.go
@@ -6,47 +6,47 @@ const (
 	montgomeryFactor = "3bd440fae918bc5ull"
 )
 
-// P is bigish num
+// P is biggish num
 var P = []limb{radixMask, radixMask, radixMask, radixMask, radixMask, radixMask, radixMask, radixMask, radixMask - 1, radixMask, radixMask, radixMask, radixMask, radixMask, radixMask, radixMask}
 
-// Copy copies n = y
+// working
+// Copies n = y
 func (n *bigNumber) decafCopy() *bigNumber {
 	c := &bigNumber{}
 	copy(c[:], n[:])
 	return c
 }
 
+//failing
 // XXX refactor, compare with Karatzuba mul, document what this does
 // n = x * y
-func decafMul(c, x, y *bigNumber) *bigNumber {
+func (n *bigNumber) decafMul(x, y *bigNumber) {
 
 	// copy so x is not directly modified
 	xx := x.decafCopy()
 
-	n := make([]dword_t, Limbs)
+	c := make([]dword_t, Limbs)
 
 	for i := uint(0); i < Limbs; i++ {
 		for j := uint(0); j < Limbs; j++ {
-			n[(i+j)%Limbs] += dword_t(y[i] * xx[j])
-		} // multiply and assing in one value
-		xx[(Limbs-1-i)^(Limbs/2)] += xx[Limbs-1-i] // assinging zeros
+			c[(i+j)%Limbs] += dword_t(y[i] * xx[j])
+		} // multiply and assigning in one value
+		xx[(Limbs-1-i)^(Limbs/2)] += xx[Limbs-1-i] // assigning zeros
 	}
 
 	// shifting for mul
-	n[Limbs-1] += n[Limbs-2] >> Radix
-	n[Limbs-2] &= dword_t(radixMask) // masked off
-	n[Limbs/2] += n[Limbs-1] >> Radix
+	c[Limbs-1] += c[Limbs-2] >> Radix
+	c[Limbs-2] &= dword_t(radixMask) // masked off
+	c[Limbs/2] += c[Limbs-1] >> Radix
 
 	for k := uint(0); k < Limbs; k++ {
-		n[k] += n[(k-1)%Limbs] >> Radix
-		n[(k-1)%Limbs] &= dword_t(radixMask)
+		c[k] += c[(k-1)%Limbs] >> Radix
+		c[(k-1)%Limbs] &= dword_t(radixMask)
 	}
 
 	for l := uint(0); l < Limbs; l++ {
-		c[l] = limb(n[l])
+		n[l] = limb(c[l])
 	}
-
-	return c
 }
 
 // XXX this should be just twice mul
@@ -56,39 +56,39 @@ func (n *bigNumber) decafSqr(x *bigNumber, y uint) *bigNumber {
 }
 
 // working
-//weak reduce mod p
-func decafWeakReduce(x *bigNumber) *bigNumber {
-	x[Limbs/2] += x[Limbs-1] >> Radix
+// Weak reduce mod p
+func (n *bigNumber) decafWeakReduce() {
+	n[Limbs/2] += n[Limbs-1] >> Radix
 	for i := uint(0); i < Limbs; i++ {
-		x[i] += x[(i-1)%Limbs] >> Radix
-		x[(i-1)%Limbs] &= radixMask
+		n[i] += n[(i-1)%Limbs] >> Radix
+		n[(i-1)%Limbs] &= radixMask
 	}
-	return x
 }
 
-// Substract mod p.
-func decafSub(n, x, y *bigNumber) *bigNumber {
-	for i := uint(0); i < Limbs; i++ {
+// working
+// Substract mod p
+// n = x = y
+func (n *bigNumber) decafSub(x, y *bigNumber) {
+	for i := 0; i < Limbs; i++ {
 		n[i] = x[i] - y[i] + 2*P[i] // maybe similar to bias?
-		decafWeakReduce(n)
 	}
-	return n
+	n.decafWeakReduce()
 }
 
-// canonicalize
-// similar to strong reduce
-func decafCanon(x *bigNumber) *bigNumber {
-	decafWeakReduce(x)
+// XXX: check if working complety
+// Canonicalize, similar to strong reduce
+func (n *bigNumber) decafCanon() {
+	n.decafWeakReduce()
 
 	// substract p with borrow
 
-	// use d_word_t instead of word_t?
+	// XXX: use d_word_t instead of word_t?
 	var carry limb
 
-	// guess there is no need for uint.. for range?
+	// XXX: guess there is no need for uint.. for range?
 	for i := uint(0); i < Limbs; i++ {
-		carry = carry + x[i] - P[i]
-		x[i] &= carry & radixMask
+		carry = carry + n[i] - P[i]
+		n[i] &= carry & radixMask
 		carry >>= Radix
 	}
 
@@ -97,38 +97,23 @@ func decafCanon(x *bigNumber) *bigNumber {
 
 	// add it back
 	for j := uint(0); j < Limbs; j++ {
-		carry += carry + x[j] + (P[j] & addback)
-		x[j] = carry & radixMask
+		carry += carry + n[j] + (P[j] & addback)
+		n[j] = carry & radixMask
 		carry >>= Radix
 	}
-
-	return x
 }
 
+//working
 // compare a == b
-func decafEq(x, y *bigNumber) dword_t {
+func decafEq(x, y *bigNumber) bool {
 	n := &bigNumber{}
-	decafSub(n, x, y)
-	decafCanon(n)
+	n.decafSub(x, y)
+	n.decafCanon()
 
 	var ret limb
 
-	for i := uint(0); i < Limbs; i++ {
+	for i := 0; i < Limbs; i++ {
 		ret |= n[i]
 	}
-
-	return dword_t(ret-1) >> 32
+	return ((dword_t(ret) - 1) >> 32) != 0
 }
-
-// should be a point
-// uint32 bool
-// are we using bools?
-//func pointEq(p, q *bigNumber) bool {
-//	a := &bigNumber{}
-//	b := &bigNumber{}
-
-//	decafMul(a, p, q)
-//	decafMul(b, p, q)
-
-//	return decafEq(a, b)
-//}
